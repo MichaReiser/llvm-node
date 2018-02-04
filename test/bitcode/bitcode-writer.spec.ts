0f603e23ca68f57cbc0f8c0875b4b9809fc8543e
Micha Reiser <micha@famreiser.ch>
2018-02-04 13:14:27 +0100

    Complete BitcodeWriter Tests

--- test/bitcode/bitcode-writer.spec.ts
+++ test/bitcode/bitcode-writer.spec.ts
@@ -4,19 +4,21 @@ const fs = require("fs");
 const filename = "./bitcode-writer-test.bc";
 
 describe("BitcodeWriter", () => {
-    let mod: llvm.Module;
-    let context: llvm.LLVMContext;
+    function setupModule() {
+        const context = new llvm.LLVMContext();
+        const mod = new llvm.Module("test", context);
+
+        return { mod, context };
+    }
 
     beforeEach(() => {
         if (fs.existsSync(filename)) {
             fs.unlinkSync(filename);
         }
-
-        context = new llvm.LLVMContext();
-        mod = new llvm.Module("test", context);
     });
 
     test("writeBitcodeToFile", () => {
+        const { mod } = setupModule();
         llvm.writeBitcodeToFile(mod, filename);
 
         expect(fs.existsSync(filename)).toBe(true);
