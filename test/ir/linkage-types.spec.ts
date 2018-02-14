42392fe4afc252263ae3094571c987bbaa538269
Micha Reiser <micha@famreiser.ch>
2018-02-14 18:14:21 +0100

    LinkageTypes Tests

---
+++ test/ir/linkage-types.spec.ts
@@ -0,0 +1,9 @@
+import * as llvm from "../../";
+
+describe("ir/linkage-types", () => {
+  for (const key of Object.keys(llvm.LinkageTypes)) {
+    it(`defines ${key}`, () => {
+      expect(llvm.LinkageTypes[key]).toBeDefined();
+    });
+  }
+});
